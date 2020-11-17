38946d8d2b65e3c8b644a10e17aa47a68ef02193
Yatsiuk Oleksandr <saj87@bk.ru>
2020-11-17 13:52:08 +0200

    Updates

---
+++ precache-manifest.6f892793663824f025e6e0ef75adb4dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb099a0817786a579e3c94e69f1ac08",
+    "url": "/Butsko/index.html"
+  },
+  {
+    "revision": "ac7b861d44aef8e9d2a4",
+    "url": "/Butsko/static/css/main.86c3638e.chunk.css"
+  },
+  {
+    "revision": "bd849d6b61d9f1b99165",
+    "url": "/Butsko/static/js/2.da0573aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Butsko/static/js/2.da0573aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7b861d44aef8e9d2a4",
+    "url": "/Butsko/static/js/main.191918d7.chunk.js"
+  },
+  {
+    "revision": "7c5994678e3ad617a9c9",
+    "url": "/Butsko/static/js/runtime-main.5d03c7a8.js"
+  },
+  {
+    "revision": "d55a49ee421e3614b1936202b6749d4c",
+    "url": "/Butsko/static/media/1.d55a49ee.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Butsko/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ef5224dfe344aed69a02efa25d64b81",
+    "url": "/Butsko/static/media/logo2.5ef5224d.png"
+  }
+]);
